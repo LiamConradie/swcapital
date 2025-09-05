7bb3e906e92b779cff1e23c44056e72c76e32a09
Liam Conradie <liam@Liams-MacBook-Pro.local>
2025-09-05 12:27:49 -0500

    Update package.json and vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // IMPORTANT for swcapital.co.uk
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
